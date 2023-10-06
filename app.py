cd786b7e9a71abb74a42eaff27b1ac401b38173d
moadams847 <moadams847@gmail.com>
2023-10-06 16:27:46 +0000

    added requirement file

--- app.py
+++ app.py
@@ -13,25 +13,13 @@ st.header('Input Features')
 
 feature1 = st.number_input('PM2_5', value=4.0)
 feature2 = st.number_input('PM10', value=4.0)
-feature3 = st.number_input('O3', value= 0.02)
-feature4 = st.number_input('CO', value=0.01)
-feature5 = st.number_input('Temp', value=24.8)
-feature6 = st.number_input('Hum', value=92.0)
-feature7 = st.number_input('ITemp', value=34.0)
-feature8 = st.number_input('CO2', value=411.0)
 
 
 # Make predictions
 if st.button('Predict'):
     input_data = pd.DataFrame({
         'PM2_5': [feature1],
-        'PM10': [feature2],
-        'O3': [feature3],
-        'CO': [feature4],
-        'Temp': [feature5],
-        'Hum': [feature6],
-        'ITemp': [feature7],
-        'CO2': [feature8]
+        'PM10': [feature2]
     })
     prediction = model.predict(input_data)
     st.header('Prediction')
